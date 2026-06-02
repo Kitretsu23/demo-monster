06633fc2fecea5dfd6f23ffd0ab9ab39a46b1a80
adiidii <sableaditi8@gmail.com>
2026-06-03 02:17:21 +0530

    rt

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def home():
 
 @app.route('/about')
 def about():
-    return "Flask is running!"
+    return "Flask is running yayaa!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
